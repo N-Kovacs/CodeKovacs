ce9a32d576ca09b9b32e8ae4d17f6540b7501182
Nicholas Kovacs <110848072+N-Kovacs@users.noreply.github.com>
2023-04-03 15:04:46 -0400

    removed react vite details

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -34,16 +34,6 @@ export default function Home(props) {
       </div>
 
       <h3>Built with Vite + React</h3>
-      <div className="card"></div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://reactjs.org" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
     </div>
   );
 }
